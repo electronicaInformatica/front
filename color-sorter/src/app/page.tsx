2f1029f4b9c2607c09099b186f84c1c4b3929e0f
maiacamarero <maia.camarero@ing.austral.edu.ar>
2024-05-17 14:45:15 -0300

    initial page with qty and buttons

--- color-sorter/src/app/page.tsx
+++ color-sorter/src/app/page.tsx
@@ -1,95 +1,83 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+import React, { useState, useEffect } from 'react';
+import styles from './page.module.css';
+import Link from "next/link";
+import Navbar from "@/components/Navbar";
+import { FaSpinner } from 'react-icons/fa';
+
+const IndexPage: React.FC = () => {
+    const [count, setCount] = useState<number>(0);
+    const [id, setId] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const generateRandomId = () => {
+            return Math.floor(10000 + Math.random() * 90000).toString();
+        };
+        setId(generateRandomId());
+    }, []);
+
+    const handleStartClick = () => {
+        setIsLoading(true)
+
+        const data = {
+            id,
+            count
+        };
+
+        console.log('Data to send:', data);
+
+        fetch('/api/count-rocklets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                setIsLoading(false);
+            });
+    };
+
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Navbar />
+            <div className={styles.container}>
+                <h1 className={styles.title}>Color Sorter</h1>
+                <a></a>
+                <div className={styles.inputContainer}>
+                    <label htmlFor="rockletCount" className={styles.label}>Quantity of Rocklets:</label>
+                    <input
+                        type="number"
+                        id="rockletCount"
+                        className={styles.input}
+                        value={count}
+                        onChange={(e) => setCount(Number(e.target.value))}
+                    />
+                </div>
+                <div className={styles.btns}>
+                    <button className={styles.btn} onClick={handleStartClick} disabled={isLoading}>Start</button>
+                    <button className={styles.btn} disabled={isLoading}>Stop</button>
+                    <Link href={"/statistics"}>
+                        <button className={styles.btn} disabled={isLoading}>Statistics</button>
+                    </Link>
+                </div>
+                {isLoading && (
+                    <div className={styles.loadingContainer}>
+                        <FaSpinner className={styles.spinner} />
+                        <p>Sorting...</p>
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
+    );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+export default IndexPage;
---
+++ color-sorter/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className={styles.navbar}>
+            <ul>
+                <li>
+                <Link  href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/statistics">Statistics</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
