0631b089a989183536d36d78918f98ec01bd29ff
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-06-07 19:37:07 -0300

    colors + remove bloat

--- color-sorter/src/app/page.tsx
+++ color-sorter/src/app/page.tsx
@@ -1,27 +1,18 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
-import {PiSpinnerGapBold} from "react-icons/pi";
 import { startSorting, stopSorting, getColors } from '@/services';
 
 const IndexPage: React.FC = () => {
     const [amountToBeSorted, setAmountToBeSorted] = useState<number>(0);
     const [sortingId, setSortingId] = useState<string>('');
-    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [colors, setColors] = useState<{ [color: string]: number }>({});
 
-    useEffect(() => {
-        const generateRandomId = () => {
-            return Math.floor(10000 + Math.random() * 90000).toString();
-        };
-        setSortingId(generateRandomId());
-    }, []);
 
     const handleStartClick = async () => {
-        setIsLoading(true)
 
         const data = {
             sortingId: sortingId,
@@ -35,7 +26,6 @@ const IndexPage: React.FC = () => {
     };
 
     const handleStopClick = async () => {
-        setIsLoading(false)
         if (sortingId) {
             await stopSorting(sortingId);
             setSortingId('');
@@ -64,30 +54,25 @@ const IndexPage: React.FC = () => {
                         onChange={(e) => setAmountToBeSorted(Number(e.target.value))}
                     />
                 </div>
-                {isLoading && (
-                    <div className={styles.loadingContainer}>
-                        <PiSpinnerGapBold className={styles.spinner} />
-                        <p>Sorting...</p>
-                    </div>
-                )}
                 <div className={styles.btns}>
-                    <button className={styles.btn} onClick={handleStartClick} disabled={isLoading}>Start</button>
+                    <button className={styles.btn} onClick={handleStartClick} >Start</button>
                     <button className={styles.btn} onClick={handleStopClick}>Stop</button>
                     <Link href={"/statistics"}>
-                        <button className={styles.btn} disabled={isLoading}>Statistics</button>
+                        <button className={styles.btn} >Statistics</button>
                     </Link>
+                    <button className={styles.btn} onClick={fetchColors}>Load sorted</button>
                 </div>
-                {/*{sortingId && (*/}
-                {/*    <div>*/}
-                {/*        <h3>Sorting ID: {sortingId}</h3>*/}
-                {/*        <h4>Colors:</h4>*/}
-                {/*        <ul>*/}
-                {/*            {Object.entries(colors).map(([color, count]) => (*/}
-                {/*                <li key={color}>{color}: {count}</li>*/}
-                {/*            ))}*/}
-                {/*        </ul>*/}
-                {/*    </div>*/}
-                {/*)}*/}
+                <div className={styles.colorsContainer}>
+                    <h2>Sorting ID: {sortingId}</h2>
+                    <ul>
+                        {Object.entries(colors).map(([color, count]) => (
+                            <li key={color} className={styles.colorItem}>
+                                <span className={styles.colorLabel} style={{backgroundColor: color}}></span>
+                                {color}: {count}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
