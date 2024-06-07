87b1557d03392829d5a7f5cfc3ba43e006fb6d64
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-06-07 09:43:23 -0300

    dotenv

--- color-sorter/src/api.ts
+++ color-sorter/src/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import dotenv from "dotenv";
+dotenv.config();
+
+let baseURL = process.env.URL;
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://' + baseURL + ':3001',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
